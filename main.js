7d389335bd752a0b138c565df47e5357835cf37a
Clupai8o0 <clupaio4@gmail.com>
2023-02-16 16:20:20 +0300

    turning the threejs code into OOP

--- main.js
+++ main.js
@@ -1,35 +1,50 @@
 import * as THREE from "three";
 
-// init
+export default class Sketch {
+	constructor(options) {
+		this.container = options.domElement;
+		this.width = this.container.offsetWidth;
+		this.height = this.container.offsetHeight;
 
-const camera = new THREE.PerspectiveCamera(
-	70,
-	window.innerWidth / window.innerHeight,
-	0.01,
-	10
-);
-camera.position.z = 1;
+		this.camera = new THREE.PerspectiveCamera(
+			70,
+			this.width / this.height,
+			0.01,
+			10
+		);
+		this.camera.position.z = 1;
 
-const scene = new THREE.Scene();
+		this.scene = new THREE.Scene();
 
-const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
-const material = new THREE.MeshNormalMaterial();
+		this.renderer = new THREE.WebGLRenderer({ antialias: true });
+		this.renderer.setSize(this.width, this.height);
 
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+		this.container.appendChild(this.renderer.domElement);
 
-const renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setAnimationLoop(animation);
+		this.time = 0;
+		this.addObjects();
+		this.render();
+	}
 
-const container = document.getElementById("container");
-container.appendChild(renderer.domElement);
+	addObjects() {
+		this.geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+		this.material = new THREE.MeshNormalMaterial();
 
-// animation
+		this.mesh = new THREE.Mesh(this.geometry, this.material);
+		this.scene.add(this.mesh);
+	}
 
-function animation(time) {
-	mesh.rotation.x = time / 2000;
-	mesh.rotation.y = time / 1000;
+	render() {
+		this.time += 0.05;
+		this.mesh.rotation.x = this.time / 2000;
+		this.mesh.rotation.y = this.time / 1000;
 
-	renderer.render(scene, camera);
+		this.renderer.render(this.scene, this.camera);
+		console.log(this.time);
+		requestAnimationFrame(this.render.bind(this));
+	}
 }
+
+new Sketch({
+	domElement: document.getElementById("container"),
+});
